5e57028c986f8d424ee0f06aa61b3b590b7a5271
Lamb11 <abc_1118@hotmail.com>
2015-02-16 23:18:38 -0600

    Se creo la pantalla para listar los bares. Se agrego la la carpeta de JSON

---
+++ cellOaxaca.h
@@ -0,0 +1,16 @@
+//
+//  cellOaxaca.h
+//  BaresOaxaca
+//
+//  Created by Alberto Cordero Arellanes on 16/02/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellOaxaca : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgUser;
+@property (strong, nonatomic) IBOutlet UILabel *lblName;
+@property (strong, nonatomic) IBOutlet UILabel *lblDescripcion;
+
+@end
